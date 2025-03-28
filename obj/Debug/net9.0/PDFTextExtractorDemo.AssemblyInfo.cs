7bef1696eb384f75eaa55515e240375674fd7733
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-28 11:16:18 +0530

    Update assembly information and add frontend dependencies for PDFTextExtractorDemo

--- obj/Debug/net9.0/PDFTextExtractorDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/PDFTextExtractorDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d190e4595d54fe579f9bbe9fe860194a7e20e7fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a79b9c12869da6e2441162cce6828939d8cd59")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
