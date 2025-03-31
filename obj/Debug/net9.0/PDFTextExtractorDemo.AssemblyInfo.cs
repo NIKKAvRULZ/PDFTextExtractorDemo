27defbb0da0b3b0ae837baae0da3f8b1a6069280
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-31 12:14:08 +0530

    Update assembly informational version and project directory path

--- obj/Debug/net9.0/PDFTextExtractorDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/PDFTextExtractorDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a06cc8ba04f73636dff4095c07d89f83db3c980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae91f84616282f6a70967e91731c258962e196cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDFTextExtractorDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
